cef2ac110ebace48b7105b914d4458cbe0dd87e5
bradleyspmckay <bradley.sp.mckay@gmail.com>
2025-07-30 22:18:25 -0500

    Added DIA button

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ document.querySelectorAll('.submit_rating').forEach(function(button) {
         const formData = new FormData(form);
         let answerString = "Answers - "
         for (let [key, value] of formData.entries()) {
-            fieldName = key.charAt(0).toUpperCase() + key.slice(1)
-            answerString += `${fieldName}: ${value} `
+            fieldName = key.charAt(0).toUpperCase() + key.slice(1);
+            answerString += `${fieldName}: ${value} `;
             console.log(answerString);
         }
         form.querySelector("p").innerText = answerString
     });
 })
+
+function openDIA() {
+    window.open("https://www.dia.mil/FOIA/FOIA-Electronic-Reading-Room/FileId/161442/")
+}
