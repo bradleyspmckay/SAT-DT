114d4b1ce98bd944f4be4fbb4bf6ff6ebee0b6f8
bradleyspmckay <bradley.sp.mckay@gmail.com>
2025-07-31 20:42:43 -0500

    Changed appearance and added sliding reports

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ document.querySelectorAll('.submit_rating').forEach(function(button) {
     button.addEventListener('click', function(event) {
         const form = button.closest('form');
         const formData = new FormData(form);
-        let answerString = "Answers - "
+        let answerString = "> "
         for (let [key, value] of formData.entries()) {
             fieldName = key.charAt(0).toUpperCase() + key.slice(1);
-            answerString += `${fieldName}: ${value} `;
+            answerString += `${fieldName}:${value} `;
             console.log(answerString);
         }
         form.querySelector("p").innerText = answerString
